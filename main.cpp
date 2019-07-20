6e532178e78882adadcb24f850c0c99dff829ee8
Nullptr <30340908+wyxhhh@users.noreply.github.com>
2019-07-21 04:54:17 +0800

    Finally hours before the competition

--- main.cpp
+++ main.cpp
@@ -15,18 +15,22 @@ is determined by the exact position of the axis;
 
 
 //----value for modification-------//
-const int turning_speed = 75;
+const int turning_speed = 55;
 const int rising_speed = 40;
-const int low_speed = 35;
-const int low_turning_speed = 10;
+const int low_speed = 45;
+const int low_turning_speed = 30; //20
 const int brushing_speed = 100;
 const int turning_time = 1280;
+const int low_rising_speed  = 20;
 //------shift gear-----------------//
 bool move_flag = true;
+vex::competition Competition;
+
+
 
 
 //------declaration of functions for the autonoumous section------------//
-void autonomous_section();
+
   // autonomous_section actually is the combination of these sub-function:
   /* In the end of each function below we add a stop function to the certain gear, 
   that is to say,the function(operation)is set to run a finite distance */
@@ -39,11 +43,151 @@ void autonomous_section();
   void brusher_down(int,int );
   void catcher_up(int,int );
   void catcher_down(int,int ); 
-void putup(int, int, int);
+void putup(int, int, int, int);
 void putdown(int, int, int, int);
 void fetchup(int, int, int, int);
 void fetchdown(int, int, int, int);
 //---------------------------------------------------------------------//
+//  automonmous part 
+void autonomous(){
+  /*moving_fwd(60, 290);
+    catcher_up(15, 600);
+    moving_fwd(60, 650);
+    
+    fetchup(35, 70, 1300, 1500);
+    catcher_up(30,700);
+	moving_fwd(60,760);
+    
+    turning_left(80, 770);
+    
+    moving_fwd(60, 970);
+    
+    moving_fwd(60,550);
+    
+    fetchup(35, 70, 1300, 1800);
+    catcher_up(30,600);
+    turning_left(50,600);
+   
+    
+    
+    moving_fwd(85,1800);
+    fetchdown(25, 50, 1500, 2000);
+    
+    moving_bwd(90,1600);*/
+    
+    moving_fwd(60, 210);
+    catcher_up(15, 600);
+    moving_fwd(60, 700);
+    
+    fetchup(35, 70, 1300, 1500);
+    catcher_up(30,700);
+	moving_fwd(60,750);
+    
+    turning_left(80, 780);
+    
+    moving_fwd(60, 1500);
+    
+    //moving_fwd(60,650);
+    
+    fetchup(40, 70, 1300, 1800); //35
+    catcher_up(30,600);
+    turning_left(50,600);
+   
+    
+    
+    moving_fwd(85,1800);
+    fetchdown(25, 75, 1500, 2000);
+    
+    moving_bwd(90,1600);
+    
+  
+};
+
+void autonomous_section2(){
+    
+  /* moving_fwd(60, 280);
+    catcher_up(15, 800);
+    moving_fwd(60, 655);
+    
+    fetchup(40, 70, 1000, 1500);
+	moving_fwd(60,760);
+    catcher_up(30,650);
+    turning_right(50, turning_time);
+    
+    moving_fwd(60, 950);
+    
+    moving_fwd(60,450);
+    
+    fetchup(40, 70, 1400, 1500);
+    catcher_up(30,600);
+    turning_right(50,600);
+    
+    moving_fwd(70,2000);
+    putdown(30,40,1000,1500);
+    fetchdown(40,30,1000,1500);
+    
+    moving_bwd(70,1600);
+    
+    
+    moving_fwd(60, 280);
+    catcher_up(15, 800);
+    moving_fwd(60, 655);
+    
+    fetchup(30, 70, 1000, 1500);
+	moving_fwd(60,760);
+    catcher_up(30,650);
+    turning_right(50, turning_time);
+    
+    moving_fwd(60, 950);
+    
+    moving_fwd(60,450);
+    
+    fetchup(30, 70, 1400, 1500);
+    catcher_up(30,600);
+    turning_left(50,600);
+    
+    
+    
+    moving_fwd(70,2000);
+    //putdown(30,40,1000,1500);
+    fetchdown(40,30,1000,1500);
+    
+    moving_bwd(70,1600); */
+    
+    moving_fwd(60, 210);
+    catcher_up(15, 600);
+    moving_fwd(60, 700);
+    
+    fetchup(35, 70, 1300, 1500);
+    catcher_up(30,700);
+	moving_fwd(60,750);
+    
+    turning_right(80, 780);
+    
+    moving_fwd(60, 1500);
+    
+    //moving_fwd(60,650);
+    
+    fetchup(40, 70, 1300, 1800); //35
+    catcher_up(30,600);
+    turning_right(50,600);
+   
+    
+    
+    moving_fwd(85,1800);
+    fetchdown(25, 75, 1500, 2000);
+    
+    moving_bwd(90,1600);
+    
+    
+    
+   
+}
+
+
+
+
+
 
 
 
@@ -73,57 +217,50 @@ void gearshift(){
 	move_flag = controller1.ButtonR1.pressing()?false:true;
 }
 
+
+
 //-----------------------------------------------------//
 
+  void pre_auto(void){
+      
+  }
 
 
-int main() {
-   //void autonomous_section();
-   /* if (Competition.isCompetitionSwitch()) {
-    // connected to a competition switch
-    Brain.Screen.print("Connected to a competition switch");
-	Competition.drivercontrol(usercontrol);
-	
-} else if (Competition.isFieldControl()) {
-    // connected to a field control system
-    Brain.Screen.print("Connected to a field control system");
-	Competition.autonomous( autonomous_section );
-	
-} else {
-    // not connected to any control system
-    Brain.Screen.print("not connected to a control system");
-} */
-   //atonomous_section();
-    //moving_fwd(90, 1400);
-    //turning_left(50,turning_time);
-    //catcher_up(40, 500);
-    //vex::task::sleep(1000);
+int main(){
+ //turning_left(80,750);
+    // autonomous_section1();
+   Competition.autonomous(autonomous);
+    Competition.drivercontrol(usercontrol);
+    pre_auto();
+    while(1){
+        vex::task::sleep(100);
+    }
     
-    //-------fetch and put down one square------------
-   /* brusher_up(70,800);    
-    catcher_up(50, 500);
-    cocatch_down(13, 30, 1250, 2000);*/
-    
-    //------------move forward to the first square-----
-    moving_fwd(60, 200);
-    catcher_up(15, 800);
-    moving_fwd(60, 655);
-    
-    fetchup(13, 60, 500, 1500);
-	
-    turning_left(50, turning_time);
-    moving_fwd(60, 855);
-    fetchup(13, 60, 500, 1500);
-    //fetchdown(13, 30, 500, 2000);
-    
+   /* while(1){   
+	   gearshift();
+       moving_part();
+       turning_part();
+       brusher_part();
+       catcher_part();
+        
+       vex::task::sleep(10);
+    }*/
+  
 }
 
+
+
+
+
+
+
+
 int func(int a){
-    return a+73;
+    return a+3;
 }
 
 int func2(int a){
-    return -a+73;
+    return -a+3; //73
 }
 
 /* int move_func(int a){
@@ -174,7 +311,7 @@ void turning_part(){
     
      //turn left and right,the deg of turning is decided by the operator
     
-          if(controller1.Axis4.value()>30){
+          if(controller1.Axis4.value()>125){
               motor_driver_Right1.spin(vex::directionType::rev,move_flag?low_turning_speed:turning_speed,velocityUnits::pct);
               motor_driver_Right2.spin(vex::directionType::fwd,move_flag?low_turning_speed:turning_speed,velocityUnits::pct);
               motor_driver_Right3.spin(vex::directionType::rev,move_flag?low_turning_speed:turning_speed,velocityUnits::pct); 
@@ -183,7 +320,7 @@ void turning_part(){
               motor_driver_Left3.spin(vex::directionType::rev,move_flag?low_turning_speed:turning_speed,velocityUnits::pct); 
               
           }
-         else if(controller1.Axis4.value()<-30){
+         else if(controller1.Axis4.value()<-126){
               motor_driver_Left1.spin(vex::directionType::fwd,move_flag?low_turning_speed:turning_speed,velocityUnits::pct);
               motor_driver_Left2.spin(vex::directionType::rev,move_flag?low_turning_speed:turning_speed,velocityUnits::pct);
               motor_driver_Left3.spin(vex::directionType::fwd,move_flag?low_turning_speed:turning_speed,velocityUnits::pct); 
@@ -192,6 +329,7 @@ void turning_part(){
               motor_driver_Right3.spin(vex::directionType::fwd,move_flag?low_turning_speed:turning_speed,velocityUnits::pct);
           }else{
               if (controller1.Axis3.value()>-15 && controller1.Axis3.value()<15)
+                  
                  stop_driver_motor();
           }
 }
@@ -200,12 +338,12 @@ void turning_part(){
 
 void brusher_part(){
     
-    if(controller1.ButtonDown.pressing()){
+    if(controller1.ButtonUp.pressing()){
         motor_brusher_left.spin(vex::directionType::rev,brushing_speed,velocityUnits::pct);
         motor_brusher_right.spin(vex::directionType::fwd,brushing_speed,velocityUnits::pct);       
     }
     
-    else if(controller1.ButtonUp.pressing()){
+    else if(controller1.ButtonDown.pressing()){
         motor_brusher_left.spin(vex::directionType::fwd,brushing_speed,velocityUnits::pct);
         motor_brusher_right.spin(vex::directionType::rev,brushing_speed,velocityUnits::pct);       
     }
@@ -217,17 +355,27 @@ void brusher_part(){
 }
 void catcher_part(){
   //CATCHING DOWN
-    if(controller1.ButtonL2.pressing()){
+    if(controller1.Axis2.value()<-35){
         motor_riser_left.spin(vex::directionType::rev,rising_speed,velocityUnits::rpm);
         motor_riser_right.spin(vex::directionType::fwd,rising_speed,velocityUnits::rpm);
     }
   //PUT IT UP
-    else if(controller1.ButtonL1.pressing()){
+    else if(controller1.Axis2.value()>35){
         motor_riser_left.spin(vex::directionType::fwd,rising_speed,velocityUnits::rpm);
         motor_riser_right.spin(vex::directionType::rev,rising_speed,velocityUnits::rpm);
     }
   // if no 1-Button is pressed, just stop the motor  
     /* if((!controller1.ButtonL1.pressing())&&(!controller1.ButtonL2.pressing())) */
+    else if(controller1.ButtonX.pressing()){
+        motor_riser_left.spin(vex::directionType::fwd,low_rising_speed,velocityUnits::rpm);
+        motor_riser_right.spin(vex::directionType::rev,low_rising_speed,velocityUnits::rpm);
+    }
+    else if(controller1.ButtonB.pressing()){
+        motor_riser_left.spin(vex::directionType::rev,low_rising_speed,velocityUnits::rpm);
+        motor_riser_right.spin(vex::directionType::fwd,low_rising_speed,velocityUnits::rpm);
+    }
+    
+    
 	else
     {  
         stop_catcher_motor();
@@ -247,35 +395,6 @@ void usercontrol(){
     }
 }
 
-void atonomous_section(){
-    /*to do operations in the 15s-
-    autonomous part of the game using 
-       the sub-functions below*/
-	moving_fwd(100, 1000);
-	turning_left(100, 500);
-	moving_fwd(40, 1000);
-	//catcher_up(40, 1000);
-	//brusher_up(100, 1000); 
-	
-	//cocatch_up(40, 100, 1000);
-	
-	turning_right(100, 500);
-	moving_fwd(100, 1000);
-	turning_right(100, 500);
-	moving_fwd(100, 1000);
-	//catcher_up(100, 400);
-	//brusher_up(100, 1000);
-	
-	//cocatch_up(40, 100, 1000);
-	
-	moving_fwd(100, 500);
-	turning_left(100, 500);
-	moving_fwd(50, 800);
-	brusher_down(50, 1000);
-	
-	//cocatch_down(50,50, 1000);
-    
-}
 
 
 void moving_fwd(int speed,int t){
@@ -354,15 +473,15 @@ void catcher_down(int speed,int t){
     stop_catcher_motor();
 }
 
-void putup(int catchspeed, int brushspeed, int t){
+void putup(int catchspeed, int brushspeed,int catcht, int t){
 	motor_brusher_left.spin(vex::directionType::rev,brushspeed,velocityUnits::pct);
     motor_brusher_right.spin(vex::directionType::fwd,brushspeed,velocityUnits::pct);
     vex::task::sleep(10);
 	motor_riser_left.spin(vex::directionType::fwd,catchspeed,velocityUnits::pct);
     motor_riser_right.spin(vex::directionType::rev,catchspeed,velocityUnits::pct);
-	
-	vex::task::sleep(t);
+	vex::task::sleep(catcht);
     stop_catcher_motor();
+    vex::task::sleep(t-catcht);
 	stop_brusher_motor();
 }
 
@@ -382,8 +501,10 @@ void  putdown(int catchspeed, int brushspeed,int catcht, int t){
 }
 
 void fetchup(int catchspeed, int brushspeed, int catcht, int t){
-    motor_brusher_left.spin(vex::directionType::rev,brushspeed,velocityUnits::pct);
+    
+    
     motor_brusher_right.spin(vex::directionType::fwd,brushspeed,velocityUnits::pct);
+    motor_brusher_left.spin(vex::directionType::rev,brushspeed,velocityUnits::pct);
     vex::task::sleep(10);
     motor_riser_left.spin(vex::directionType::rev,catchspeed,velocityUnits::pct);
     motor_riser_right.spin(vex::directionType::fwd,catchspeed,velocityUnits::pct);
@@ -397,13 +518,13 @@ void fetchup(int catchspeed, int brushspeed, int catcht, int t){
 }
 
 void fetchdown(int catchspeed, int brushspeed, int catcht, int t){
-    motor_brusher_left.spin(vex::directionType::rev,brushspeed,velocityUnits::pct);
-    motor_brusher_right.spin(vex::directionType::fwd,brushspeed,velocityUnits::pct);
+    motor_brusher_left.spin(vex::directionType::fwd,brushspeed,velocityUnits::pct);
+    motor_brusher_right.spin(vex::directionType::rev,brushspeed,velocityUnits::pct);
     vex::task::sleep(10);
-    motor_riser_left.spin(vex::directionType::rev,catchspeed,velocityUnits::pct);
-    motor_riser_right.spin(vex::directionType::fwd,catchspeed,velocityUnits::pct);
+   // motor_riser_left.spin(vex::directionType::rev,catchspeed,velocityUnits::pct);
+    //motor_riser_right.spin(vex::directionType::fwd,catchspeed,velocityUnits::pct);
     vex::task::sleep(catcht);
-    stop_catcher_motor();
+    //stop_catcher_motor();
     motor_riser_left.spin(vex::directionType::fwd,catchspeed,velocityUnits::pct);
     motor_riser_right.spin(vex::directionType::rev,catchspeed,velocityUnits::pct);
 	vex::task::sleep(t-catcht);
